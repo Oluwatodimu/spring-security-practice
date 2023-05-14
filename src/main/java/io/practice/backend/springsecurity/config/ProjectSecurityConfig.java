6c780a96966055e30bb79635c1dbcf478609c239
todimuisewon <todimuisewon@gmail.com>
2023-05-14 18:49:01 +0100

    added custom security configurations

--- src/main/java/io/practice/backend/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/io/practice/backend/springsecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package io.practice.backend.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +19,22 @@ public class ProjectSecurityConfig {
                 .and().formLogin()
                 .and().httpBasic();
         return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("user")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
