227cf18f3b963cde60b5bd37dcb7649595649fa0
todimuisewon <todimuisewon@gmail.com>
2023-05-17 05:07:13 +0100

    configured UserDetails to be returned from the db, making it robust

--- src/main/java/io/practice/backend/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/io/practice/backend/springsecurity/config/ProjectSecurityConfig.java
@@ -3,9 +3,8 @@ package io.practice.backend.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,28 +12,23 @@ public class ProjectSecurityConfig {
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests()
+        httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests()
                 .requestMatchers("/api/v1/account", "/api/v1/balance", "/api/v1/loans", "/api/v1/cards").authenticated()
-                .requestMatchers("/api/v1/notice", "/api/v1/contact").permitAll()
+                .requestMatchers("/api/v1/notice", "/api/v1/contact", "/api/v1/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return httpSecurity.build();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .authorities("user")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
---
+++ src/main/java/io/practice/backend/springsecurity/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package io.practice.backend.springsecurity.repository;
+
+import io.practice.backend.springsecurity.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByEmail(String email);
+}
