1e49b6d8d5b8acaab8da7a0b6055305d2aeeb4d5
todimuisewon <todimuisewon@gmail.com>
2023-05-19 05:39:46 +0100

    worked on models

--- src/main/java/io/practice/backend/springsecurity/model/Customer.java
+++ src/main/java/io/practice/backend/springsecurity/model/Customer.java
@@ -1,26 +1,32 @@
 package io.practice.backend.springsecurity.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 
-import java.io.Serializable;
-
 @Entity
-public class Customer implements Serializable {
+public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    @Column(name = "customer_id")
     private int id;
+
+    private String name;
 
     private String email;
 
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String pwd;
 
     private String role;
+
+    @Column(name = "create_dt")
+    private String createDt;
 
     public int getId() {
         return id;
@@ -30,12 +36,28 @@ public class Customer implements Serializable {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
     }
 
     public String getPwd() {
@@ -52,5 +74,13 @@ public class Customer implements Serializable {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(String createDt) {
+        this.createDt = createDt;
     }
 }
